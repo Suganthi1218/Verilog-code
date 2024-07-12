621651db4a8e5d2e4af684df3b60e1c193b2f095
Suganthi1218 <suganthi1218@gmail.com>
2024-07-12 16:11:43 +0530

    Update and rename ha_tb.v to halfadder_tb.v

---
+++ day 1/half adder/halfadder_tb.v
@@ -0,0 +1,22 @@
+
+module halfadder_tb;
+
+reg a,b;
+wire sum,carry;
+
+halfadder ha(a,b,sum,carry);
+
+initial 
+
+begin 
+
+a=0; b=0; #10
+
+a=0; b=1; #10
+
+a=1; b=0; #10
+
+a=1; b=1; 
+
+end 
+endmodule 
